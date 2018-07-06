6a7a2658d26128e9bb6d0167718f481b512c8130
Daniel Limanowski <dlimanow@iastate.edu>
2018-07-05 22:29:38 -0500

    added css/style handling, reworked packages

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # django-react-boilerplate
+A boilerplate to integrate the following technologies:
+Django (backend)
+React JS (frontend)
+Redux (state management for React)
+
+The goal for this boilerplate is to provide a simple template that encourages
+rapid, pain-free development of a modern, single-page web application.
+
+Feel free to open issues with problems, bugs, or questions. I will do my best
+to address them.
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,11 @@
 const path = require('path')
 const webpack = require('webpack')
-const BundleTracker = require('webpack-bundle-tracker')
+//const BundleTracker = require('webpack-bundle-tracker')
 
 module.exports = {
     context: __dirname,
 
-    entry: [
-        'webpack-dev-server/client?http://localhost:3000',
-        'webpack/hot/only-dev-server',
-        './project/assets/js/index',
-    ],
+    entry: './project/assets/js/index.js',
     
     output: {
         path: path.resolve('./project/assets/bundles/'),
@@ -21,19 +17,22 @@ module.exports = {
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoEmitOnErrorsPlugin(), // Prevent reload upon discovery of an error
-        new BundleTracker({filename: './project/webpack-stats.json'}),
+       // new BundleTracker({filename: './project/webpack-stats.json'}),
     ],
 
     module: {
         rules: [
             { // Below tells Babel to convert JSX to JS that browsers can understand
-                test: /\.jsx?$/, 
+                test: /\.js?$/, 
                 exclude: /node_modules/,
                 use: [
-                    { loader: 'react-hot' },
                     { loader: 'babel-loader' },
                 ]
             },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
         ]
     },
 
